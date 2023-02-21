5700f3e33e2160c38e3acacc850a83a7c8d2dc80
Hana Jirovska <h.jirovska@student.tudelft.nl>
2023-02-21 15:09:56 +0100

    Try using heuristics, time node schedule construction

--- solve.py
+++ solve.py
@@ -1,3 +1,5 @@
+import time
+
 from pycsp3 import *
 from node_schedule import NodeSchedule
 from activity_metadata import ActiveSet
@@ -5,12 +7,15 @@ from activity_metadata import ActiveSet
 if __name__ == '__main__':
     # TODO: make this input into a command-line argument
     active = ActiveSet.create_active_set(["../session_configs/qkd.yaml"], [[1, 2, 3, 4, 5]])
+    # active = ActiveSet.create_active_set(["../session_configs/qkd.yaml", "../session_configs/bqc-client.yaml"],
+    #                                      [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
 
-    schedule_size = 30  # TODO: how to define schedule size
+    schedule_size = 80  # TODO: how to define schedule size
     capacities = [1, 1]
 
     # x[i] is the starting time of the ith job
     x = VarArray(size=active.n_blocks, dom=range(schedule_size))
+    # TODO: already set the initial domains more efficiently??
 
     # taken from http://pycsp.org/documentation/models/COP/RCPSP/
     def cumulative_for(k):
@@ -23,8 +28,11 @@ if __name__ == '__main__':
         # precedence constraints
         [x[i] + active.durations[i] <= x[j] for i in range(active.n_blocks) for j in active.successors[i]],
         # resource constraints
-        [cumulative_for(k) <= capacity for k, capacity in enumerate(capacities)]
-        # TODO: constraints for min and max time lags
+        [cumulative_for(k) <= capacity for k, capacity in enumerate(capacities)],
+        # constraints for max time lags
+        [(x[i+1] - (x[i] + active.durations[i])) <= active.d_max[i+1] for i in range(active.n_blocks - 1)]
+        # constraint for min time lags, TODO: this currently doesn't make sense I think, double-check the time lags
+        # [active.d_min[i+1] <= (x[i+1] - (x[i] + active.durations[i])) for i in range(active.n_blocks - 1)]
     )
 
     # optional objective function
@@ -37,8 +45,13 @@ if __name__ == '__main__':
     ace = solver(ACE)
 
     # TODO: this is where you define heuristics I think
-    result = ace.solve(instance)
+    start = time.time()
+    # https://github.com/xcsp3team/pycsp3/blob/master/docs/optionsSolvers.pdf
+    result = ace.solve(instance, dict_options={"valh": "max"})
+    # result = ace.solve(instance)
+    end = time.time()
 
     if status() is SAT:
         ns = NodeSchedule(active.n_blocks, solution().values, active.durations, active.resource_reqs)
         ns.print()
+        print("\nTime taken to finish: %.4f seconds" % (end - start))
