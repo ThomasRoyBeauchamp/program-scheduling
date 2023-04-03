9e4e8cc4f9120d457503124e2162056bdf59ad0a
Hana Jirovska <h.jirovska@student.tudelft.nl>
2023-04-03 18:23:21 +0200

    Add a script for retrieving tasks from Qoala and creating a YAML template

---
+++ retrieve_qoala_tasks.py
@@ -0,0 +1,115 @@
+import os
+import yaml
+from typing import List
+
+from qoala.lang.ehi import UnitModule
+from qoala.lang.parse import QoalaParser
+from qoala.lang.program import QoalaProgram
+from qoala.runtime.config import (
+    ProcNodeNetworkConfig,
+    ProcNodeConfig,
+    TopologyConfig,
+    LatenciesConfig
+)
+from qoala.runtime.environment import NetworkInfo
+from qoala.runtime.program import ProgramInput, BatchInfo
+from qoala.sim.build import build_network
+
+sourcecode = "https://github.com/QuTech-Delft/qoala-sim/blob/" \
+             "635728c60ded03e84f254ab413071a6392e19a7c/tests/integration/pingpong/test_pingpong.py"
+
+
+# copied from `sourcecode`
+def load_program(path: str) -> QoalaProgram:
+    path = os.path.join(os.path.dirname(__file__), path)
+    with open(path) as file:
+        text = file.read()
+    return QoalaParser(text).parse()
+
+
+# copied from `sourcecode`
+def create_network_info(names: List[str]) -> NetworkInfo:
+    env = NetworkInfo.with_nodes({i: name for i, name in enumerate(names)})
+    env.set_global_schedule([0, 1, 2])
+    env.set_timeslot_len(1e6)
+    return env
+
+
+# copied from `sourcecode`
+def create_procnode_cfg(name: str, id: int, num_qubits: int) -> ProcNodeConfig:
+    return ProcNodeConfig(
+        node_name=name,
+        node_id=id,
+        topology=TopologyConfig.perfect_config_uniform_default_params(num_qubits),
+        latencies=LatenciesConfig(
+            host_instr_time=500, host_peer_latency=100_000, qnos_instr_time=1000
+        ),
+    )
+
+
+def create_batch(
+        program: QoalaProgram,
+        unit_module: UnitModule,
+        inputs: List[ProgramInput],
+        num_iterations: int,
+) -> BatchInfo:
+    return BatchInfo(
+        program=program,
+        unit_module=unit_module,
+        inputs=inputs,
+        num_iterations=num_iterations,
+        deadline=0,
+    )
+
+
+def retrieve_tasks(config, num_qubits=3):
+    # TODO: maybe this could be done with program instances instead idk
+    network_info = create_network_info(names=["bob", "alice"])
+    alice_id = network_info.get_node_id("alice")
+    bob_id = network_info.get_node_id("bob")
+
+    alice_node_cfg = create_procnode_cfg("alice", alice_id, num_qubits)
+    bob_node_cfg = create_procnode_cfg("bob", bob_id, num_qubits)
+
+    network_cfg = ProcNodeNetworkConfig.from_nodes_perfect_links(
+        nodes=[alice_node_cfg, bob_node_cfg], link_duration=500_000
+    )
+    network = build_network(network_cfg, network_info)
+    alice_procnode = network.nodes["alice"]
+    bob_procnode = network.nodes["bob"]
+
+    alice_program = load_program(config + "_alice.iqoala")
+    alice_inputs = [ProgramInput({"bob_id": bob_id})]
+
+    alice_unit_module = UnitModule.from_full_ehi(alice_procnode.memmgr.get_ehi())
+    alice_batch = create_batch(
+        alice_program, alice_unit_module, alice_inputs, 1
+    )
+    alice_procnode.submit_batch(alice_batch)
+    alice_procnode.initialize_processes()
+    alice_tasks = alice_procnode.scheduler.get_tasks_to_schedule()
+
+    with open(config + '_alice.yml', 'w') as outfile:
+        yaml.dump({"session_id": "TODO",
+                   "app_deadline": "TODO",
+                   "blocks": [{bt.block_name: {"type": bt.typ.name, "duration": bt.duration, "CS": "TODO"}}
+                              for bt in alice_tasks]}, outfile, default_flow_style=False, sort_keys=False)
+
+    bob_program = load_program(config + "_bob.iqoala")
+    bob_inputs = [ProgramInput({"alice_id": alice_id})]
+
+    bob_unit_module = UnitModule.from_full_ehi(bob_procnode.memmgr.get_ehi())
+    bob_batch = create_batch(bob_program, bob_unit_module, bob_inputs, 1)
+    bob_procnode.submit_batch(bob_batch)
+    bob_procnode.initialize_processes()
+    bob_tasks = bob_procnode.scheduler.get_tasks_to_schedule()
+
+    with open(config + '_bob.yml', 'w') as outfile:
+        yaml.dump({"session_id": "TODO",
+                   "app_deadline": "TODO",
+                   "blocks": [{bt.block_name: {"type": bt.typ.name, "duration": bt.duration, "CS": "TODO"}}
+                              for bt in bob_tasks]}, outfile, default_flow_style=False, sort_keys=False)
+
+
+if __name__ == '__main__':
+    retrieve_tasks("configs/pingpong")
