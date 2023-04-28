089bb26a4d57ac8cd8166bd0be421af751d6eab1
Hana Jirovska <h.jirovska@student.tudelft.nl>
2023-04-28 16:18:00 +0200

    Add checks for divisibility

--- datasets.py
+++ datasets.py
@@ -11,25 +11,33 @@ def create_dataset(id, n_sessions):
     elif id == 2:
         return {path + "qkd": n_sessions}
     elif id == 3:
+        if n_sessions % 2 != 0:
+            raise ValueError(f"Number of session is not divisible by 2 ({n_sessions}), pick a different dataset.")
         return {
-            path + "bqc": n_sessions / 2,
-            path + "pingpong": n_sessions / 2,
+            path + "bqc": int(n_sessions / 2),
+            path + "pingpong": int(n_sessions / 2),
         }
     elif id == 4:
+        if n_sessions % 2 != 0:
+            raise ValueError(f"Number of session is not divisible by 2 ({n_sessions}), pick a different dataset.")
         return {
-            path + "bqc": n_sessions / 2,
-            path + "qkd": n_sessions / 2,
+            path + "bqc": int(n_sessions / 2),
+            path + "qkd": int(n_sessions / 2),
         }
     elif id == 5:
+        if n_sessions % 2 != 0:
+            raise ValueError(f"Number of session is not divisible by 2 ({n_sessions}), pick a different dataset.")
         return {
-            path + "qkd": n_sessions / 2,
-            path + "pingpong": n_sessions / 2,
+            path + "qkd": int(n_sessions / 2),
+            path + "pingpong": int(n_sessions / 2),
         }
     elif id == 6:
+        if n_sessions % 3 != 0:
+            raise ValueError(f"Number of session is not divisible by 3 ({n_sessions}), pick a different dataset.")
         return {
-            path + "bqc": n_sessions / 3,
-            path + "pingpong": n_sessions / 3,
-            path + "qkd": n_sessions / 3
+            path + "bqc": int(n_sessions / 3),
+            path + "pingpong": int(n_sessions / 3),
+            path + "qkd": int(n_sessions / 3)
         }
     else:
         print(f"Dataset ID {id} not recognised.")
